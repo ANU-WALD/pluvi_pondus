21b4f347a002060281aefc8d03cf42cfa44db458
prl900 <pablo.larraondo@anu.edu.au>
2018-06-28 13:27:18 +1000

    Script to download GPM data added

--- data_sync/early_gpm.sh
+++ data_sync/early_gpm.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
-DOI=$1
+#YESTERDAY
+DATE=`date -d "yesterday 12:00" +%Y%m%d`
 
-DATE=`date +%Y%m`
-
-FTP_PATH='ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/early/'
-OUTDIR='/g/data/fj4/SatellitePrecip/GPM/global/early/'$DOI
-
-if [ -d "$OUTDIR" ]; then
-	echo $OUTDIR$DATE
-	# mkdir $OUTDIR
-fi
+FTP_PATH='ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/early/'${DATE:0:6}
+OUTDIR='/g/data/fj4/SatellitePrecip/GPM/global/early/'${DATE:0:6}
 
 if [ ! -d "$OUTDIR" ]; then
-	echo $OUTDIR$DATE
+ 	echo "Creating dir"$OUTDIR
+	mkdir $OUTDIR
 fi
+
+cd $OUTDIR
+curl -l $FTP_PATH'/*' --user $NRT_ACCOUNT:$NRT_ACCOUNT | while read NAME; do 
+        if [[ $NAME == 3B-HHR-E.MS.MRG.3IMERG.$DATE* ]] && [ ! -f $OUTDIR'/'$NAME ]; then
+    		echo $NAME" Does not exist!"
+		curl -O $FTP_PATH'/'$NAME --user $NRT_ACCOUNT:$NRT_ACCOUNT
+	fi
+done
+
+
+#TODAY
+DATE=`date +%Y%m%d`
+
+FTP_PATH='ftp://jsimpson.pps.eosdis.nasa.gov/NRTPUB/imerg/early/'${DATE:0:6}
+OUTDIR='/g/data/fj4/SatellitePrecip/GPM/global/early/'${DATE:0:6}
+
+if [ ! -d "$OUTDIR" ]; then
+ 	echo "Creating dir"$OUTDIR
+	mkdir $OUTDIR
+fi
+
+cd $OUTDIR
+curl -l $FTP_PATH'/*' --user $NRT_ACCOUNT:$NRT_ACCOUNT | while read NAME; do 
+        if [[ $NAME == 3B-HHR-E.MS.MRG.3IMERG.$DATE* ]] && [ ! -f $OUTDIR'/'$NAME ]; then
+    		echo $NAME" Does not exist!"
+		curl -O $FTP_PATH'/'$NAME --user $NRT_ACCOUNT:$NRT_ACCOUNT
+	fi
+done
+
