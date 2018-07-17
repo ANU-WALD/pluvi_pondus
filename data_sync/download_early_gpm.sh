239f36948e572b0f23ae8364172eda2768cf44da
prl900 <pablo.larraondo@anu.edu.au>
2018-07-17 21:00:08 +1000

    Download ERA5 pressure level fields

--- data_sync/download_early_gpm.sh
+++ data_sync/download_early_gpm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-in=2015-01-01
-while [ "$in" != 2018-07-10 ]; do
+in=2018-07-01
+while [ "$in" != 2018-07-20 ]; do
   in=$(date -I -d "$in + 1 day")
   x=$(date -d "$in" +%Y%m%d)
+  ./final_gpm.sh $x
+  ./early_gpm.sh $x
   ./final_gpm.sh $x
 done
--- data_sync/download_era5_geop.py
+++ data_sync/download_era5_geop.py
@@ -14,7 +14,7 @@ def monthly_range(start_date, end_date):
         
 server = ECMWFDataServer()
 
-start_date = date(2018, 5, 1)
+start_date = date(2018, 1, 1)
 end_date = date(2018, 7, 1)
 for (d0, d1) in monthly_range(start_date, end_date):
     server.retrieve({
@@ -23,12 +23,13 @@ for (d0, d1) in monthly_range(start_date, end_date):
         "dataset": "era5",
         "date": "{0}/to/{1}".format(d0.strftime('%Y-%m-%d'), d1.strftime('%Y-%m-%d')),
         "expver": "1",
-        "levtype": "sfc",
-        "param": "228.128/260015",
-        "step": "1/2/3/4/5/6/7/8/9/10/11/12",
+        "levtype": "pl",
+	"levelist": "500/800/1000",
+        "param": "129.128/130.128/157.128",
+        "step": "0/1/2/3/4/5/6/7/8/9/10/11",
         "stream": "oper",
         "time": "06:00:00/18:00:00",
         "grid": "0.25/0.25",
         "format": "netcdf",
-        "target": "/g/data1/fj4/ERA5/precip_{0}.nc".format(d0.strftime('%Y%m')),
+        "target": "/g/data/fj4/ECMWF/ERA5/press_levels_{0}.nc".format(d0.strftime('%Y%m')),
     })
