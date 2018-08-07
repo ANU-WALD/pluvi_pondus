bc7498a7969948de675dc87602b46502207c4f3c
prl900 <pablo.larraondo@anu.edu.au>
2018-08-07 12:44:58 +1000

    GPC aggregation script now accumulates up to the time

--- data_pipeline/gpm_aggregator.py
+++ data_pipeline/gpm_aggregator.py
@@ -4,36 +4,49 @@ import numpy as np
 from datetime import datetime
 from datetime import timedelta
 
-def get_gpm_filepaths(start, accum_steps):
+def get_gpm_filepath(end):
     pattern = "/g/data/fj4/SatellitePrecip/GPM/global/final/{ym}/3B-HHR.MS.MRG.3IMERG.{ymd}-S{sHMS}-E{eHMS}.{mins_day:04d}.V05B.HDF5"
+    start = end - timedelta(minutes=30)
     
-    paths = []
-    for _ in range(accum_steps):
-        paths.append(pattern.format(ym=start.strftime('%Y%m'), ymd=start.strftime('%Y%m%d'), sHMS=start.strftime('%H%M%S'), eHMS=(start+timedelta(minutes=29, seconds=59)).strftime('%H%M%S'), mins_day=start.hour*60+start.minute))
-        start = start + timedelta(minutes=30)
-
-    return paths
+    return pattern.format(ym=start.strftime('%Y%m'), ymd=start.strftime('%Y%m%d'), sHMS=start.strftime('%H%M%S'), eHMS=(start+timedelta(minutes=29, seconds=59)).strftime('%H%M%S'), mins_day=start.hour*60+start.minute)
 
 
-def aggregate_gpm(start, end):
-    
-    with netCDF4.Dataset("/g/data/fj4/SatellitePrecip/GPM/global/final/prec_{}.nc".format(start.strftime('%Y%m')), 'w', format='NETCDF4') as dest:
-        timestamps = np.arange(start, end, timedelta(hours=3)).astype(datetime)
+def get_accum_prec(end, accum_h):
+    timestamps = np.arange(end - timedelta(hours=accum_h), end, timedelta(hours=1)).astype(datetime) + timedelta(hours=1)
+    accum = np.zeros((1800, 3600), dtype=np.float)
 
-        prec_arr = np.empty((timestamps.shape[0], 1800, 3600), dtype=np.float32)
-        prec_accum = np.empty((1800, 3600), dtype=np.float32)
+    for t in timestamps:
+        f_path = get_gpm_filepath(t)
+        with h5py.File(f_path, mode='r') as f:
+            prec = f['Grid']['precipitationCal'][:].T
+            prec[prec == -9999.9] = np.nan
+            accum = accum + prec
 
-        for i, t in enumerate(timestamps):  
-            prec_accum.fill(0)
-            for filepath in get_gpm_filepaths(t, 6):
-                with h5py.File(filepath, mode='r') as f:
-                    prec = f['Grid']['precipitationCal'][:].T
-                    prec[prec==-9999.9] = np.nan
-                    prec_accum = prec_accum + prec
+    return accum
 
-            prec_arr[i, :, :] = prec_accum / 2
-            
-        print(start)
+
+def get_temporal_accum(timestamps, accum_h):
+    prec_arr = np.empty((timestamps.shape[0], 1800, 3600), dtype=np.float32)
+
+    for i, t in enumerate(timestamps):
+        acc = get_accum_prec(t, accum_h)
+        prec_arr[i, :, :] = acc / 2
+
+    return prec_arr
+
+
+def get_month_range(year, month, accum_h):
+    start_date = datetime(year, month, 1, 3, 0)
+    end_date = start_date + timedelta(days=33)
+    end_date = end_date.replace(day=1)
+
+    return np.arange(start_date, end_date, timedelta(hours=accum_h)).astype(datetime)
+
+
+def aggregate_gpm(year, month, accum_h):
+    with netCDF4.Dataset("/g/data/fj4/SatellitePrecip/GPM/global/final/prec_{}h_accum_{}{:02d}.nc".format(accum_h, year, month), 'w', format='NETCDF4') as dest:
+        timestamps = get_month_range(year, month, accum_h)
+        prec_accum = get_temporal_accum(timestamps, accum_h)
 
         setattr(dest, "date_created", datetime.now().strftime("%Y%m%dT%H%M%S"))
         setattr(dest, "Conventions", "CF-1.6")
@@ -43,11 +56,11 @@ def aggregate_gpm(start, end):
         t_dim = dest.createDimension("time", prec_arr.shape[0])
 
         var = dest.createVariable("time", "f8", ("time",))
-        var.units = "seconds since 1970-01-01 00:00:00.0"
+        var.units = "seconds since 1900-01-01 00:00:00.0"
         var.calendar = "standard"
         var.long_name = "Time, unix time-stamp"
         var.standard_name = "time"
-        var[:] = netCDF4.date2num(timestamps, units="seconds since 1970-01-01 00:00:00.0", calendar="standard")
+        var[:] = netCDF4.date2num(timestamps, units="seconds since 1900-01-01 00:00:00.0", calendar="standard")
 
         var = dest.createVariable("longitude", "f8", ("longitude",))
         var.units = "degrees_east"
@@ -62,7 +75,7 @@ def aggregate_gpm(start, end):
         var = dest.createVariable("precipitationCal", "f4", ("time", "latitude", "longitude"), fill_value=-9999.9, zlib=True, chunksizes=(8, 400, 400))
         var.long_name = "Precipitation Calibrated"
         var.units = 'mm'
-        var[:] = prec_arr
+        var[:] = prec_accum
 
         
 from dateutil.relativedelta import relativedelta
