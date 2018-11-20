4f6512b09c74aa5d683523c94a59ed7512c58880
prl900 <pablo.larraondo@anu.edu.au>
2018-11-20 13:47:20 +1100

    era interim scripts added

--- cnn_test/era5_geop_packer.sh
+++ cnn_test/era5_geop_packer.sh
@@ -11,16 +11,18 @@ while [ "$DATE" != "$END" ]; do
 		echo "PROBLEM"
 		exit 1
 	fi
-	cdo selname,z $FILE /g/data/fj4/scratch/tmpz.nc
-	cdo sellevel,500 /g/data/fj4/scratch/tmpz.nc /g/data/fj4/scratch/tmpz500.nc
+	cdo selname,z $FILE /g/data/fj4/scratch/tmpz1.nc
+	cdo selhour,0,6,12,18 /g/data/fj4/scratch/tmpz1.nc /g/data/fj4/scratch/tmpz2.nc
+	#cdo sellevel,500 /g/data/fj4/scratch/tmpz.nc /g/data/fj4/scratch/tmpz500.nc
 	#cdo sellonlatbox,110.0,158.0,-9.0,-45.0 /g/data/fj4/scratch/tmpz.nc /g/data/fj4/scratch/tmpz_$(date +%Y%m%d%H%M -d "$DATE").nc
-	cdo sellonlatbox,-50.0,40.0,75.0,15.0 /g/data/fj4/scratch/tmpz.nc /g/data/fj4/scratch/infile.nc
-	rm /g/data/fj4/scratch/tmpz.nc
-	rm /g/data/fj4/scratch/tmpz500.nc
-	cdo remapcon,outgrid.txt /g/data/fj4/scratch/infile.nc /g/data/fj4/scratch/eu_tmpz_$(date +%Y%m%d%H%M -d "$DATE").nc
-	rm /g/data/fj4/scratch/infile.nc
+	#cdo sellonlatbox,-50.0,40.0,75.0,15.0 /g/data/fj4/scratch/tmpz2.nc /g/data/fj4/scratch/infile.nc
+	cdo sellonlatbox,-50.0,40.0,75.0,15.0 /g/data/fj4/scratch/tmpz2.nc /g/data/fj4/scratch/eu_tmpz_$(date +%Y%m%d%H%M -d "$DATE").nc
+	rm /g/data/fj4/scratch/tmpz?.nc
+	#cdo remapcon,outgrid.txt /g/data/fj4/scratch/infile.nc /g/data/fj4/scratch/eu_tmpz_$(date +%Y%m%d%H%M -d "$DATE").nc
+	#rm /g/data/fj4/scratch/infile.nc
 	DATE=$(date -ud "$DATE + 1 month")
 done
 
-cdo -b f32 mergetime /g/data/fj4/scratch/eu_tmpz_*.nc /g/data/fj4/scratch/EU_Z_ERA5.nc
+#cdo -b f32 mergetime /g/data/fj4/scratch/eu_tmpz_*.nc /g/data/fj4/scratch/EU_Z_ERA5.nc
+cdo -b f32 mergetime /g/data/fj4/scratch/eu_tmpz_*.nc /g/data/fj4/scratch/EU_Z_SPATIAL_FULL_ERA5.nc
 rm /g/data/fj4/scratch/eu_tmpz_*.nc
--- cnn_test/nc2npy.py
+++ cnn_test/nc2npy.py
@@ -1,10 +1,10 @@
 from netCDF4 import Dataset
 import numpy as np
 
-tp = Dataset("/g/data/fj4/scratch/EU_TP_ERA5.nc", "r")["tp"][:]
+tp = Dataset("/g/data/fj4/scratch/EU_TP_ERAI.nc", "r")["tp"][:]
 print(tp.shape)
-np.save("/g/data/fj4/scratch/eu_y", tp.data.astype(np.float32))
+np.save("/g/data/fj4/scratch/eu_erai_y", tp.data.astype(np.float32))
 
-z = Dataset("/g/data/fj4/scratch/EU_Z_ERA5.nc", "r")["z"][:, 1, :, :]
+z = Dataset("/g/data/fj4/scratch/EU_Z_ERAI.nc", "r")["z"][:, 1, :, :]
 print(z.shape)
-np.save("/g/data/fj4/scratch/eu_x", z.data.astype(np.float32))
+np.save("/g/data/fj4/scratch/eu_erai_x", z.data.astype(np.float32))
