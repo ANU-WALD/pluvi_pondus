89e0f6a7228cc61494cec5889d6fc724a85c869f
prl900 <pablo.larraondo@anu.edu.au>
2019-01-23 15:23:02 +1100

    Ready to test Himawari

--- cnn_test/4chan_encoder.py
+++ cnn_test/4chan_encoder.py
@@ -1,20 +1,25 @@
 from keras.models import Sequential
 from keras.layers import BatchNormalization, Conv2D, Conv2DTranspose
 from keras.optimizers import Adam
+import sys
 import numpy as np
 
 def GetModel():
     model = Sequential()
-    model.add(BatchNormalization(axis=3, input_shape=(80, 120, 1)))
-    model.add(Conv2D(64, (5, 5), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3, input_shape=(240, 360, 2)))
+    model.add(Conv2D(32, (5, 5), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    model.add(Conv2D(64, (3, 3), strides=(2, 2), activation='relu', padding='same'))
     model.add(BatchNormalization(axis=3))
     model.add(Conv2D(128, (3, 3), strides=(2, 2), activation='relu', padding='same'))
     model.add(BatchNormalization(axis=3))
-    model.add(Conv2D(256, (3, 3), strides=(2, 2), activation='relu', padding='same'))
+    model.add(Conv2D(256, (5, 5), strides=(3, 3), activation='relu', padding='same'))
     model.add(BatchNormalization(axis=3))
-    model.add(Conv2DTranspose(128, (3, 3), strides=(2, 2), activation='relu', padding='same'))
+    model.add(Conv2DTranspose(128, (5, 5), strides=(3, 3), activation='relu', padding='same'))
     model.add(BatchNormalization(axis=3))
     model.add(Conv2DTranspose(64, (3, 3), strides=(2, 2), activation='relu', padding='same'))
+    model.add(BatchNormalization(axis=3))
+    model.add(Conv2DTranspose(32, (3, 3), strides=(2, 2), activation='relu', padding='same'))
     model.add(BatchNormalization(axis=3))
     model.add(Conv2DTranspose(1, (5, 5), strides=(2, 2), activation='relu', padding='same'))
 
@@ -26,18 +31,27 @@ def GetModel():
 
     return model
 
-x = np.expand_dims(np.load("/scratch/director2107/ERA5_Data/ERA5_HIM8/era5_z.npy"), axis=3)
-y = 1000*np.expand_dims(np.load("/scratch/director2107/ERA5_Data/ERA5_HIM8/era5_tp.npy"), axis=3)
-
+#x = np.moveaxis(np.load("/home/pl5189/ERA5_HIM8_CNN/era5_z.npy"), 1, -1)[:,:240,:360,:]
+#np.save("/home/pl5189/ERA5_HIM8_CNN/era5_z_rec.npy", x)
+#him8 = np.load("/home/pl5189/ERA5_HIM8_CNN/him8_b8.npy")
+#np.save("/home/pl5189/ERA5_HIM8_CNN/era5_z_aux.npy", x)
+#x = np.load("/home/pl5189/ERA5_HIM8_CNN/era5_z_rec.npy")[:,:,:,2,None].copy()
+era5 = np.load("/home/pl5189/ERA5_HIM8_CNN/era5_z_aux.npy")[:]
+him8 = np.load("/home/pl5189/ERA5_HIM8_CNN/him8_b7.npy")[1:, :, :, None]
+x = np.stack([him8, era5], axis=3)
 print(x.shape)
-print(y.shape)
+era5 = None
+him8 = None
+#y = np.load("/home/pl5189/ERA5_HIM8_CNN/era5_tp_rec.npy")[:26338,:]
+y = np.load("/home/pl5189/ERA5_HIM8_CNN/era5_tp_rec.npy")[1:,:]
 
-for i in range(10):
-    x_train = x[:9000, :]
-    y_train = y[:9000, :]
 
-    x_test = x[9000:, :]
-    y_test = y[9000:, :]
 
-    model = GetModel()
-    history = model.fit(x_train, y_train, epochs=10, verbose=1, validation_data=(x_test, y_test))
+x_train = x[:20000, :]
+y_train = y[:20000, :]
+
+x_test = x[20000:, :]
+y_test = y[20000:, :]
+
+model = GetModel()
+history = model.fit(x_train, y_train, batch_size=24, epochs=10, verbose=1, validation_data=(x_test, y_test))
