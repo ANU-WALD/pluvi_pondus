4670d4b121005e72c7a431db38695d47a622a0f4
prl900 <pablo.larraondo@anu.edu.au>
2019-08-22 15:42:25 +1000

    added data generator

--- rainfields/encdec_missingvals.py
+++ rainfields/encdec_missingvals.py
@@ -1,23 +1,74 @@
 from keras import layers
 from keras import models
 from keras.layers import BatchNormalization, Conv2D, UpSampling2D, MaxPooling2D, Dropout
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
+from keras.utils import Sequence
 import numpy as np
 from keras import backend as K
 import tensorflow as tf
+import xarray as xr
+from datetime import datetime
+from datetime import timedelta
+import random
+import os
 
 def mse_holes(y_true, y_pred):
-    idxs = K.tf.where(k.tf.math.is_nan(y_true))
-    y_true = K.gather(y_true, idxs)
-    y_pred = K.gather(y_pred, idxs)
+    idxs = K.tf.where(K.tf.math.logical_not(K.tf.math.is_nan(y_true)))
+    y_true = K.tf.gather_nd(y_true, idxs)
+    y_pred = K.tf.gather_nd(y_pred, idxs)
 
     return K.mean(K.square(y_true-y_pred), axis=-1)
 
-def get_unet(loss):
-    concat_axis = 3
-    inputs = layers.Input(shape = (2050, 2450, 4))
+class DataGenerator(Sequence):
+    def __init__(self, batch_size=4, length=40):
+        'Initialization'
+        self.batch_size = batch_size
+        self.length = length
 
-    feats = 32
+    def __len__(self):
+        return int(self.length/self.batch_size)
+
+    def __getitem__(self, index):
+        x = []
+        y = []
+
+        while len(y) < self.batch_size:
+            n = random.randint(1,6*24*6)
+            d = datetime(2018,11,1,0,0) + timedelta(0,10*60*n)
+            dp = d - timedelta(0,10*60)
+            rf_fp = "/data/pluvi_pondus/Rainfields/{}/310_{}_{}00.prcp-c10.npy".format(int(d.strftime("%d")), d.strftime("%Y%m%d"), d.strftime("%H%M"))
+            b8_fp = "/data/pluvi_pondus/HIM8_AU_{}_B8_{}.npy".format(d.strftime("%Y%m%d"), d.strftime("%H%M%S"))
+            b14_fp = "/data/pluvi_pondus/HIM8_AU_{}_B14_{}.npy".format(d.strftime("%Y%m%d"), d.strftime("%H%M%S"))
+            b8p_fp = "/data/pluvi_pondus/HIM8_AU_{}_B8_{}.npy".format(dp.strftime("%Y%m%d"), dp.strftime("%H%M%S"))
+            b14p_fp = "/data/pluvi_pondus/HIM8_AU_{}_B14_{}.npy".format(dp.strftime("%Y%m%d"), dp.strftime("%H%M%S"))
+            
+            if not os.path.exists(rf_fp) or not os.path.exists(b8_fp) or not os.path.exists(b8p_fp):
+                continue
+
+            b8 = np.load(b8_fp)[2::2, 402::2]
+            b14 = np.load(b14_fp)[2::2, 402::2]
+            b8p = np.load(b8p_fp)[2::2, 402::2]
+            b14p = np.load(b14p_fp)[2::2, 402::2]
+            
+            prec = np.load(rf_fp)[2::2, 402::2]
+
+            x.append(np.stack((b8p,b14p,b8,b14), axis=-1))
+            y.append(prec)
+
+        x = np.stack(x, axis=0)
+        y = np.stack(y, axis=0)[:,:,:,None]
+
+        return x, y
+
+    def on_epoch_end(self):
+        pass
+
+
+def get_unet():
+    concat_axis = 3
+    inputs = layers.Input(shape = (1024, 1024, 4))
+
+    feats = 16
     bn0 = BatchNormalization(axis=3)(inputs)
     conv1 = layers.Conv2D(feats, (3, 3), activation='relu', padding='same', name='conv1_1')(bn0)
     bn1 = BatchNormalization(axis=3)(conv1)
@@ -40,21 +91,34 @@ def get_unet(loss):
     bn7 = BatchNormalization(axis=3)(conv4)
     conv4 = layers.Conv2D(8*feats, (3, 3), activation='relu', padding='same')(bn7)
     bn8 = BatchNormalization(axis=3)(conv4)
-    pool4 = layers.MaxPooling2D(pool_size=(2, 3))(bn8)
+    pool4 = layers.MaxPooling2D(pool_size=(2, 2))(bn8)
 
     conv5 = layers.Conv2D(16*feats, (3, 3), activation='relu', padding='same')(pool4)
     bn9 = BatchNormalization(axis=3)(conv5)
     conv5 = layers.Conv2D(16*feats, (3, 3), activation='relu', padding='same')(bn9)
     bn10 = BatchNormalization(axis=3)(conv5)
+    pool5 = layers.MaxPooling2D(pool_size=(2, 2))(bn10)
 
-    up_conv5 = layers.UpSampling2D(size=(2, 3))(bn10)
-    up6 = layers.concatenate([up_conv5, conv4], axis=concat_axis)
-    conv6 = layers.Conv2D(8*feats, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = layers.Conv2D(32*feats, (3, 3), activation='relu', padding='same')(pool5)
     bn11 = BatchNormalization(axis=3)(conv6)
-    conv6 = layers.Conv2D(8*feats, (3, 3), activation='relu', padding='same')(bn11)
+    conv6 = layers.Conv2D(32*feats, (3, 3), activation='relu', padding='same')(bn11)
     bn12 = BatchNormalization(axis=3)(conv6)
 
     up_conv6 = layers.UpSampling2D(size=(2, 2))(bn12)
+    up7 = layers.concatenate([up_conv6, conv5], axis=concat_axis)
+    conv7 = layers.Conv2D(16*feats, (3, 3), activation='relu', padding='same')(up7)
+    bn13 = BatchNormalization(axis=3)(conv6)
+    conv7 = layers.Conv2D(16*feats, (3, 3), activation='relu', padding='same')(bn12)
+    bn14 = BatchNormalization(axis=3)(conv6)
+    
+    up_conv5 = layers.UpSampling2D(size=(2, 2))(bn10)
+    up6 = layers.concatenate([up_conv5, conv4], axis=concat_axis)
+    conv6 = layers.Conv2D(8*feats, (3, 3), activation='relu', padding='same')(up6)
+    bn15 = BatchNormalization(axis=3)(conv6)
+    conv6 = layers.Conv2D(8*feats, (3, 3), activation='relu', padding='same')(bn15)
+    bn16 = BatchNormalization(axis=3)(conv6)
+
+    up_conv6 = layers.UpSampling2D(size=(2, 2))(bn16)
     up7 = layers.concatenate([up_conv6, conv3], axis=concat_axis)
     conv7 = layers.Conv2D(4*feats, (3, 3), activation='relu', padding='same')(up7)
     bn13 = BatchNormalization(axis=3)(conv7)
@@ -81,57 +145,16 @@ def get_unet(loss):
     model = models.Model(inputs=inputs, outputs=conv10)
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss=loss, optimizer=sgd, metrics=['mse','mae', closs.get_pod_loss(.1), closs.get_pom_loss(.1), closs.get_far_loss(.1), closs.get_pofd_loss(.1)])
+    model.compile(loss=mse_holes, optimizer=sgd)
     print(model.summary())
 
     return model
 
+training_gen = DataGenerator(batch_size=4, length=400)
+validation_gen = DataGenerator(batch_size=4, length=24)
 
-# Levels [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100]
-#x = x[:, :, :, ]
-#x = x[:, :, :, [0,2,5]]
-#x = np.load("/data/ERA-Int/10zlevels.npy")[:, :, :, [0,2,5]]
-x = np.load("/data/ERA-Int/10zlevels_min.npy")
-print(x.shape)
-y = np.load("/data/ERA-Int/tp_min.npy")
-print(y.shape, y.min(), y.max())
-
-idxs = np.arange(x.shape[0])
-np.random.seed(0)
-np.random.shuffle(idxs)
-
-x = x[idxs, :, :, :]
-x_train = x[:14000, :]
-x_test = x[14000:, :]
-
-y = y[idxs, :, :, None]
-y_train = y[:14000, :]
-y_test = y[14000:, :]
-
-print(x_train.shape, y_train.shape)
-"""
-losses = {'pom_mae_raw': closs.get_diff_pom_mae_loss(.5), 'pofd_mae_raw': closs.get_diff_pofd_mae_loss(.5),'pom_mse_raw': closs.get_diff_pom_mse_loss(.5), 'pofd_mse_raw': closs.get_diff_pofd_mse_loss(.5), 'mae_raw': 'mae', 'mse_raw': 'mse'}
-
-for name, loss in losses.items():
-    print(name)
-    model = get_unet(loss)
-    history = model.fit(x_train, y_train, epochs=200, batch_size=32, validation_data=(x_test, y_test))
-    with open('train_history_unet_{}_10lvels.pkl'.format(name), 'wb') as f:
-        pickle.dump(history.history, f)
-    model.save('unet_{}_10levels.h5'.format(name))
-"""
-
-y_train = np.log(1+y_train)
-y_test = np.log(1+y_test)
-
-#losses = {'pom_mae': closs.get_diff_pom_mae_loss(.5), 'pofd_mae': closs.get_diff_pofd_mae_loss(.5),'pom_mse': closs.get_diff_pom_mse_loss(.5), 'pofd_mse': closs.get_diff_pofd_mse_loss(.5), 'mae': 'mae', 'mse': 'mse'}
-#losses = {'far_mae': closs.get_diff_far_mae_loss(.5),'pom_mse': closs.get_diff_pom_mse_loss(.5), 'far_mse': closs.get_diff_far_mse_loss(.5), 'pofd_mse': closs.get_diff_pofd_mse_loss(.5), 'comb_mae': closs.get_diff_comb_mae_loss(.5), 'comb_mse': closs.get_diff_comb_mse_loss(.5, .1, .1)}
-losses = {'comb_mse_11': closs.get_diff_comb_mse_loss(.5, .1, .1), 'comb_mse_19': closs.get_diff_comb_mse_loss(.5, .1, .9), 'comb_mse_91': closs.get_diff_comb_mse_loss(.5, .9, .1), 'comb_mse_99': closs.get_diff_comb_mse_loss(.5, .9, .9)}
-
-for name, loss in losses.items():
-    print(name)
-    model = get_unet(loss)
-    history = model.fit(x_train, y_train, epochs=200, batch_size=32, validation_data=(x_test, y_test))
-    with open('train_history_unet_{}_10lvels.pkl'.format(name), 'wb') as f:
-        pickle.dump(history.history, f)
-    model.save('unet_{}_10levels.h5'.format(name))
+model = get_unet()
+#history = model.fit(x_train, y_train, epochs=50, batch_size=4, validation_data=(x_test, y_test))
+#history = model.fit_generator(generator=training_gen, validation_data=validation_gen, use_multiprocessing=True, workers=2)
+history = model.fit_generator(generator=training_gen, validation_data=validation_gen, epochs=100, max_queue_size=8, use_multiprocessing=True, workers=4)
+model.save('rainfields_model.h5')
