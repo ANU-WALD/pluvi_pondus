056cb9015b8dfcaf6f348a586f0f441d5dff3b5a
prl900 <pablo.larraondo@anu.edu.au>
2019-10-22 14:46:59 +1100

    Tested small scale model

--- rainfields/small/unet_gan_generator_1gpu.py
+++ rainfields/small/unet_gan_generator_1gpu.py
@@ -7,13 +7,13 @@ import tensorflow as tf
 import os
 import time
 
+import matplotlib.pyplot as plt
+
 from nc_loader_small import HimfieldsDataset
 
-def Unet():
+def Generator():
     concat_axis = 3
-    #inputs = layers.Input(shape = (256, 256, 2))
     inputs = layers.Input(shape = (1024, 1024, 4))
-    #inputs = layers.Input(shape = (2048,2048,4))
 
     feats = 16
     bn0 = layers.BatchNormalization(axis=3)(inputs)
@@ -88,81 +88,161 @@ def Unet():
     conv9 = layers.Conv2D(feats, (3, 3), activation='relu', padding='same')(bn17)
     bn18 = layers.BatchNormalization(axis=3)(conv9)
 
-    conv10 = layers.Conv2D(1, (1, 1))(bn18)
+    conv10 = layers.Conv2D(1, (1, 1), activation='relu')(bn18)
     #bn19 = BatchNormalization(axis=3)(conv10)
 
-    model = tf.keras.models.Model(inputs=inputs, outputs=conv10)
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
 
 
     return model
 
+def downsample(filters, size, apply_batchnorm=True):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                             kernel_initializer=initializer, use_bias=False))
+
+  if apply_batchnorm:
+    result.add(tf.keras.layers.BatchNormalization())
+
+  result.add(tf.keras.layers.LeakyReLU())
+
+  return result
+
+def Discriminator():
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  tar = tf.keras.layers.Input(shape=[None, None, 1], name='target_image')
+  inp = tf.keras.layers.Input(shape=[None, None, 4], name='input_image')
+
+  x = tf.keras.layers.concatenate([inp, tar])
+
+  down1 = downsample(64, 4, False)(x)
+  down2 = downsample(128, 4)(down1)
+  down3 = downsample(256, 4)(down2)
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3)
+  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                kernel_initializer=initializer,
+                                use_bias=False)(zero_pad1)
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)
+  last = tf.keras.layers.Conv2D(1, 4, strides=1, kernel_initializer=initializer)(zero_pad2)
+
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+
+LAMBDA = 100
+
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+  
+  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss
+
+def generator_loss(disc_generated_output, gen_output, target):
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+  # mean absolute error
+  #l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+  l1_loss = mse_holes(target, gen_output)
+
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+  #total_gen_loss = l1_loss
+
+  return total_gen_loss
+
+
+generator = Generator()
+discriminator = Discriminator()
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
 def mse_holes(y_true, y_pred):
-    #idxs = K.tf.where(K.tf.math.logical_not(K.tf.math.is_nan(y_true)))
     idxs = tf.where(tf.math.logical_not(tf.math.is_nan(y_true)))
     y_true = tf.gather_nd(y_true, idxs)
     y_pred = tf.gather_nd(y_pred, idxs)
 
     return K.mean(K.square(y_true-y_pred), axis=-1)
-
-def mse_holes(y_true, y_pred):
-    #idxs = K.tf.where(K.tf.math.logical_not(K.tf.math.is_nan(y_true)))
-    idxs = tf.where(tf.math.logical_not(tf.math.is_nan(y_true)))
-    y_true = tf.gather_nd(y_true, idxs)
-    y_pred = tf.gather_nd(y_pred, idxs)
-
-    return K.mean(K.square(y_true-y_pred), axis=-1)
-
-
-@tf.function
-def train_step(model, inputs, outputs, optimizer):
-
-  with tf.GradientTape() as t:
-    loss = mse_holes(outputs, model(inputs, training=True))
-
-    grads = t.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
 
 @tf.function
 def calc_loss(model, inputs, outputs):
     return mse_holes(outputs, model(inputs))
 
+@tf.function
+def train_step(input_image, target):
+  target_masked = tf.where(tf.math.is_nan(target), tf.zeros_like(target), target)
 
-def train(train_dataset, test_dataset):
-  model = Unet()
-  optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    gen_output = generator(input_image, training=True)
+    gen_output_masked = tf.where(tf.math.is_nan(target), tf.zeros_like(target), gen_output)
+    
+    disc_real_output = discriminator([input_image, target_masked], training=True)
+    disc_generated_output = discriminator([input_image, gen_output_masked], training=True)
+    disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
 
-  for epoch in range(100):
-    print("EPOCH: {}".format(epoch))
-    train_loss = tf.keras.metrics.Mean()
-    test_loss = tf.keras.metrics.Mean()
+    gen_loss = generator_loss(disc_generated_output, gen_output, target)
 
-    for (batch, (inputs, outputs)) in enumerate(train_dataset):
-      #current_loss = mse_holes(outputs, model(inputs))
-      train_step(model, inputs, outputs, optimizer)
-      #loss = mse_holes(outputs, model(inputs))
-      train_loss(calc_loss(model, inputs, outputs))
+  generator_gradients = gen_tape.gradient(gen_loss, generator.trainable_variables)
+  discriminator_gradients = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
-    print("Average training loss:", train_loss.result())
+  generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(discriminator_gradients, discriminator.trainable_variables))
+
+import numpy as np
+
+def plot_output(epoch, model, inputs, target):
+    print("Ref min: {}, max: {}, mean: {}".format(np.nanmin(target[0,:,:,0]), np.nanmax(target[0,:,:,0]), np.nanmean(target[0,:,:,0])))
+    masked = np.where(np.isnan(target[0,:,:,0]), np.nan, model(inputs)[0,:,:,0]) 
+    print("Out min: {}, max: {}, mean: {}".format(np.nanmin(masked), np.nanmax(masked), np.nanmean(masked)))
+
+    plt.imsave("ref_{:03d}.png".format(epoch), target[0,:,:,0])
+    plt.imsave("out_{:03d}.png".format(epoch), model(inputs)[0,:,:,0])
+    plt.imsave("in_{:03d}.png".format(epoch), inputs[0,:,:,0])
+
+def fit(train_ds, test_ds, epochs):
+  train_loss = tf.keras.metrics.Mean()
+  test_loss = tf.keras.metrics.Mean()
+  template = 'Epoch {}, Loss: {:.4f}, Test Loss: {:.4f}\n'
+
+  f = open("train_record.out","w+")
+
+  for epoch in range(epochs):
+    start = time.time()
+
+    # Train
+    for batch, (inputs, target) in enumerate(train_ds):
+      train_step(inputs, target)
+      train_loss(calc_loss(generator, inputs, target))
     
     epoch_loss_avg = tf.keras.metrics.Mean()
-    for (inputs, outputs) in test_dataset:
-      test_loss(calc_loss(model, inputs, outputs))
+    for batch, (inputs, target) in enumerate(test_ds):
+      test_loss(calc_loss(generator, inputs, target))
 
-    print("Average test loss:", test_loss.result())
+    print(template.format(epoch+1, train_loss.result(), test_loss.result()))
+    f.write(template.format(epoch+1, train_loss.result(), test_loss.result()))
+    f.flush() 
+   
     train_loss.reset_states()
     test_loss.reset_states()
 
+    plot_output(epoch, generator, inputs, target)
 
+    print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1, time.time()-start))
 
-"""
-train_fnames = ["/home/lar116/project/pablo/rainfields_data/H8_2B_BoM_20181101.nc",
-                "/home/lar116/project/pablo/rainfields_data/H8_2B_BoM_20181102.nc",
-                "/home/lar116/project/pablo/rainfields_data/H8_2B_BoM_20181103.nc",
-                "/home/lar116/project/pablo/rainfields_data/H8_2B_BoM_20181104.nc",
-                "/home/lar116/project/pablo/rainfields_data/H8_2B_BoM_20181105.nc",
-                "/home/lar116/project/pablo/rainfields_data/H8_2B_BoM_20181106.nc"]
-"""
+  f.close()
+  generator.save('gan_model.h5')
+
 
 train_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181101.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181102.nc",
@@ -171,6 +251,9 @@ train_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181101.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181105.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181106.nc"]
 
-training_gen = HimfieldsDataset(train_fnames, 1, batch_size=6)
-test_gen = HimfieldsDataset(train_fnames, 1, batch_size=6)
-train(training_gen, test_gen)
+
+train_dataset = HimfieldsDataset(train_fnames, 1, batch_size=4)
+test_dataset = HimfieldsDataset(train_fnames, 1, batch_size=4)
+EPOCHS = 100
+fit(train_dataset, test_dataset, EPOCHS)
+
