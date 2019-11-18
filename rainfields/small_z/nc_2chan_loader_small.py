d4623e196f82720fbc5fde475fbd9e485d8514f8
prl900 <pablo.larraondo@anu.edu.au>
2019-11-18 14:18:11 +1100

    testing geopotential

--- rainfields/small_z/nc_2chan_loader_small.py
+++ rainfields/small_z/nc_2chan_loader_small.py
@@ -8,11 +8,12 @@ import random
 tf.compat.v1.enable_eager_execution()
 
 class gen:
-    def __call__(self, fname, mult, jitter=False):
+    def __call__(self, fname, z=False, jitter=False):
         dsg = xr.open_dataset(fname.decode("utf-8"))
-        dsz1000 = xr.open_dataset("/data/pluvi_pondus/ERA5/au_z1000_201811.nc")
-        dsz800 = xr.open_dataset("/data/pluvi_pondus/ERA5/au_z800_201811.nc")
-        dsz500 = xr.open_dataset("/data/pluvi_pondus/ERA5/au_z500_201811.nc")
+        if z:
+            dsz1000 = xr.open_dataset("/data/pluvi_pondus/ERA5/au_z1000_201811.nc")
+            dsz800 = xr.open_dataset("/data/pluvi_pondus/ERA5/au_z800_201811.nc")
+            dsz500 = xr.open_dataset("/data/pluvi_pondus/ERA5/au_z500_201811.nc")
 
         for t in dsg.time:
             d = datetime.datetime.utcfromtimestamp(t.astype(int) * 1e-9)
@@ -21,26 +22,32 @@ class gen:
                 continue
             if np.datetime64(d) not in dsg.time.data:
                 continue
-
-            z500 = dsz500['z'].sel(time=t, method='nearest').data[:, 50:]
-            z800 = dsz800['z'].sel(time=t, method='nearest').data[:, 50:]
-            z1000 = dsz1000['z'].sel(time=t, method='nearest').data[:, 50:]
+            if z:
+                z500 = dsz500['z'].sel(time=t, method='nearest').data[:, 50:]
+                z800 = dsz800['z'].sel(time=t, method='nearest').data[:, 50:]
+                z1000 = dsz1000['z'].sel(time=t, method='nearest').data[:, 50:]
 
             rf_fp = "/data/pluvi_pondus/Rainfields/310_{}.prcp-c10.nc".format(d.strftime("%Y%m%d_%H%M%S"))
             dsp = xr.open_dataset(rf_fp)
             prec = dsp['precipitation'].data[2::2, 402::2]
             b8 = dsg['B8'].sel(time=t).data[2::2, 402::2]
             b14 = dsg['B14'].sel(time=t).data[2::2, 402::2]
-            
-            yield (np.stack((b8,b14), axis=-1), np.stack((z1000,z800,z500), axis=-1), prec[:, :, None])
+           
+            if z:
+                yield (np.stack((b8,b14), axis=-1), np.stack((z1000,z800,z500), axis=-1), prec[:, :, None])
+            else:
+                yield (np.stack((b8,b14), axis=-1), prec[:, :, None])
 
         dsg.close()
 
 
-def HimfieldsDataset(fnames, mult, batch_size=2):
+def HimfieldsDataset(fnames, z, batch_size=2):
 
     ds = tf.data.Dataset.from_tensor_slices(fnames)
-    ds = ds.interleave(lambda fname: tf.data.Dataset.from_generator(gen(), (tf.float32, tf.float32, tf.float32), (tf.TensorShape([1024, 1024, 2]), tf.TensorShape([256,256,3]), tf.TensorShape([1024, 1024, 1])), args=(fname, mult)), cycle_length=len(fnames), block_length=1, num_parallel_calls=None)
+    if z:
+        ds = ds.interleave(lambda fname: tf.data.Dataset.from_generator(gen(), (tf.float32, tf.float32, tf.float32), (tf.TensorShape([1024, 1024, 2]), tf.TensorShape([256,256,3]), tf.TensorShape([1024, 1024, 1])), args=(fname, z)), cycle_length=len(fnames), block_length=1, num_parallel_calls=None)
+    else:
+        ds = ds.interleave(lambda fname: tf.data.Dataset.from_generator(gen(), (tf.float32, tf.float32), (tf.TensorShape([1024, 1024, 2]), tf.TensorShape([1024, 1024, 1])), args=(fname, z)), cycle_length=len(fnames), block_length=1, num_parallel_calls=None)
     ds = ds.shuffle(128, seed=None)
     ds = ds.batch(batch_size)
 
--- rainfields/small_z/unet_func_datasetape_1gpu.py
+++ rainfields/small_z/unet_func_datasetape_1gpu.py
@@ -11,12 +11,11 @@ from nc_2chan_loader_small import HimfieldsDataset
 
 def Unet():
     concat_axis = 3
-    #inputs = layers.Input(shape = (256, 256, 2))
-    inputs = layers.Input(shape = (1024, 1024, 2))
-    #inputs = layers.Input(shape = (2048,2048,4))
+    ref_input = layers.Input(shape = (1024, 1024, 2))
+    z_input = layers.Input(shape = (256, 256, 3))
 
     feats = 16
-    bn0 = layers.BatchNormalization(axis=3)(inputs)
+    bn0 = layers.BatchNormalization(axis=3)(ref_input)
     conv1 = layers.Conv2D(feats, (3, 3), activation='relu', padding='same', name='conv1_1')(bn0)
     bn1 = layers.BatchNormalization(axis=3)(conv1)
     conv1 = layers.Conv2D(feats, (3, 3), activation='relu', padding='same')(bn1)
@@ -27,18 +26,22 @@ def Unet():
     conv2 = layers.Conv2D(2*feats, (3, 3), activation='relu', padding='same')(bn3)
     bn4 = layers.BatchNormalization(axis=3)(conv2)
     pool2 = layers.MaxPooling2D(pool_size=(2, 2))(bn4)
+    
+    zadd = layers.concatenate([z_input, pool2], axis=concat_axis)
 
-    conv3 = layers.Conv2D(4*feats, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = layers.Conv2D(4*feats, (3, 3), activation='relu', padding='same')(zadd)
     bn5 = layers.BatchNormalization(axis=3)(conv3)
     conv3 = layers.Conv2D(4*feats, (3, 3), activation='relu', padding='same')(bn5)
     bn6 = layers.BatchNormalization(axis=3)(conv3)
     pool3 = layers.MaxPooling2D(pool_size=(2, 2))(bn6)
+   
 
     conv4 = layers.Conv2D(8*feats, (3, 3), activation='relu', padding='same')(pool3)
     bn7 = layers.BatchNormalization(axis=3)(conv4)
     conv4 = layers.Conv2D(8*feats, (3, 3), activation='relu', padding='same')(bn7)
     bn8 = layers.BatchNormalization(axis=3)(conv4)
     pool4 = layers.MaxPooling2D(pool_size=(2, 2))(bn8)
+
 
     conv5 = layers.Conv2D(16*feats, (3, 3), activation='relu', padding='same')(pool4)
     bn9 = layers.BatchNormalization(axis=3)(conv5)
@@ -91,8 +94,7 @@ def Unet():
     conv10 = layers.Conv2D(1, (1, 1), activation='relu')(bn18)
     #bn19 = BatchNormalization(axis=3)(conv10)
 
-    model = tf.keras.models.Model(inputs=inputs, outputs=conv10)
-
+    model = tf.keras.models.Model(inputs=[ref_input,z_input], outputs=conv10)
 
     return model
 
@@ -117,48 +119,16 @@ def msle_holes(y_true, y_pred):
 def train_step(model, inputs, outputs, optimizer):
 
   with tf.GradientTape() as t:
-    loss = msle_holes(outputs, model(inputs, training=True))
+    loss = mse_holes(outputs, model(inputs, training=True))
 
     grads = t.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
 
 @tf.function
 def calc_loss(model, inputs, outputs):
     return mse_holes(outputs, model(inputs))
 
-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
-
-color_list = np.array([(255, 255, 255),  # 0.0
-                       (245, 245, 255),  # 0.2
-                       (180, 180, 255),  # 0.5
-                       (120, 120, 255),  # 1.5
-                       (20,  20, 255),   # 2.5
-                       (0, 216, 195),    # 4.0
-                       (0, 150, 144),    # 6.0
-                       (0, 102, 102),    # 10
-                       (255, 255,   0),  # 15
-                       (255, 200,   0),  # 20
-                       (255, 150,   0),  # 30
-                       (255, 100,   0),  # 40
-                       (255,   0,   0),  # 50
-                       (200,   0,   0),  # 60
-                       (120,   0,   0),  # 75
-                       (40,   0,   0)])  # > 100
-
-color_list = color_list/255.
-cm = LinearSegmentedColormap.from_list("BOM-RF3", color_list, N=32)
-
-def plot_output(epoch, model, inputs, target):
-    print("Ref min: {}, max: {}, mean: {}".format(np.nanmin(target[0,:,:,0]), np.nanmax(target[0,:,:,0]), np.nanmean(target[0,:,:,0])))
-    masked = np.where(np.isnan(target[0,:,:,0]), np.nan, model(inputs)[0,:,:,0]) 
-    print("Out min: {}, max: {}, mean: {}".format(np.nanmin(masked), np.nanmax(masked), np.nanmean(masked)))
-
-    plt.imsave("ref_{:03d}.png".format(epoch), target[0,:,:,0], vmin=0, vmax=10, cmap=cm)
-    plt.imsave("out_{:03d}.png".format(epoch), model(inputs)[0,:,:,0], vmin=0, vmax=10, cmap=cm)
-    plt.imsave("in_{:03d}.png".format(epoch), inputs[0,:,:,0])
 
 def fit(train_ds, test_ds, epochs):
   model = Unet()
@@ -174,42 +144,21 @@ def fit(train_ds, test_ds, epochs):
     start = time.time()
 
     # Train
-    for batch, (inputs, target) in enumerate(train_ds):
-      train_step(model, inputs, target, optimizer)
-      train_loss(calc_loss(model, inputs, target))
+    for batch, (ref_input, z_input, target) in enumerate(train_ds):
+      train_step(model, [ref_input, z_input], target, optimizer)
+      train_loss(calc_loss(model, [ref_input, z_input], target))
    
 
-    for batch, (inputs, target) in enumerate(test_ds[0]):
-      test_loss(calc_loss(model, inputs, target))
-    
-    print(template.format(epoch+1, train_loss.result(), test_loss.result()))
-    f.write(template.format(epoch+1, train_loss.result(), test_loss.result()))
-    f.flush() 
-    test_loss.reset_states()
-    
-    for batch, (inputs, target) in enumerate(test_ds[1]):
-      test_loss(calc_loss(model, inputs, target))
-    
-    print(template.format(epoch+1, train_loss.result(), test_loss.result()))
-    f.write(template.format(epoch+1, train_loss.result(), test_loss.result()))
-    f.flush() 
-    test_loss.reset_states()
-    
-    for batch, (inputs, target) in enumerate(test_ds[2]):
-      test_loss(calc_loss(model, inputs, target))
-    
-    print(template.format(epoch+1, train_loss.result(), test_loss.result()))
-    f.write(template.format(epoch+1, train_loss.result(), test_loss.result()))
-    f.flush() 
+    for batch, (ref_input, z_input, target) in enumerate(test_ds):
+      test_loss(calc_loss(model, [ref_input, z_input], target))
+   
 
     print(template.format(epoch+1, train_loss.result(), test_loss.result()))
     f.write(template.format(epoch+1, train_loss.result(), test_loss.result()))
     f.flush() 
-   
+    
     train_loss.reset_states()
     test_loss.reset_states()
-
-    plot_output(epoch, model, inputs, target)
 
     print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1, time.time()-start))
 
@@ -219,22 +168,19 @@ def fit(train_ds, test_ds, epochs):
 
 train_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181101.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181102.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181103.nc"]
-
-test1_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181104.nc"]
-test2_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181105.nc"]
-test3_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181106.nc"]
-
-"""
+                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181103.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181104.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181105.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181106.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181107.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181108.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181109.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181110.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181111.nc",
-                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181112.nc",
+                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181106.nc"]
+
+test_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181107.nc",
+               "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181108.nc",
+               "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181109.nc",
+               "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181110.nc",
+               "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181111.nc",
+               "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181112.nc"]
+
+"""
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181113.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181114.nc",
                 "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181115.nc",
@@ -267,11 +213,8 @@ test_fnames = ["/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181120.nc",
                "/data/pluvi_pondus/HIM8_AU_2B/HIM8_2B_AU_20181130.nc"]
 """
 
-train_dataset = HimfieldsDataset(train_fnames, 1, batch_size=4)
-
-test1_dataset = HimfieldsDataset(test1_fnames, 1, batch_size=4)
-test2_dataset = HimfieldsDataset(test2_fnames, 1, batch_size=4)
-test3_dataset = HimfieldsDataset(test3_fnames, 1, batch_size=4)
+train_dataset = HimfieldsDataset(train_fnames, True, batch_size=4)
+test_dataset = HimfieldsDataset(test_fnames, True, batch_size=4)
 
 EPOCHS = 15
-fit(train_dataset, [test1_dataset,test2_dataset,test3_dataset], EPOCHS)
+fit(train_dataset, test_dataset, EPOCHS)
